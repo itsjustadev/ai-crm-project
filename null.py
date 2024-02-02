851f1081e42c46ea7d3dc141af2811bdddbe09df
itsjustadev <86054371+itsjustadev@users.noreply.github.com>
2024-02-02 21:32:44 +0300

    Delete null.py

--- null.py
+++
@@ -1,9 +0,0 @@
-data = [{'role': 'user', 'content': 'afaffffaf'},
-        {'role': 'assistant', 'content': 'asafhaha'},
-        {'role': 'user', 'content': 'agahrhr'}]
-string_to_be_short=''
-for entry in data:
-    if entry['role'] == 'user':
-        string_to_be_short = string_to_be_short + (f"Клиент: {entry['content']} ")
-    elif entry['role'] == 'assistant':
-        string_to_be_short = string_to_be_short + (f"Ты: {entry['content']} ")
